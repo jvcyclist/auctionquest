878f4c192f1c7669daee265fad897ebf9e3c7f88
Patryk <patrykaras1@gmail.com>
2018-10-25 22:03:46 +0200

    Yols

--- src/main/java/pl/karas/auctionquest/controller/AQController.java
+++ src/main/java/pl/karas/auctionquest/controller/AQController.java
@@ -3,18 +3,17 @@ package pl.karas.auctionquest.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 
 import pl.karas.auctionquest.datecalc.Datec;
 
 @Controller
 @RequestMapping("/")
-@ResponseBody
+
 public class AQController {
 
 
 @RequestMapping("/calcDate")
-@ResponseBody
 public String calcDate(@ModelAttribute Datec datec) {
 	datec.setResult();
 	
@@ -22,7 +21,6 @@ public String calcDate(@ModelAttribute Datec datec) {
 }
 
 @RequestMapping("/calcDates")
-@ResponseBody
 public String calcDates() {
 	
 	
